376517b635f0c518b82f3b05ca8ee52f3d85de74
Damian Bachórz <damian.bachorz@hotmail.com>
2016-11-22 00:20:36 +0100

    error fixes

--- TicTacToe/GameBoard.xaml.cs
+++ TicTacToe/GameBoard.xaml.cs
@@ -28,5 +28,60 @@ namespace TicTacToe
             this.myLogin = myLogin;
             InitializeComponent();
         }
+
+        private void a1_Click(object sender, RoutedEventArgs e)
+        {
+            //a1.SetValue(NameProperty, 'X');
+            gameLogicLayer.sendClickedField("a1");
+            a1.Content = gameLogicLayer.myTurn;
+        }
+
+        private void a2_Click(object sender, RoutedEventArgs e)
+        {
+            //a1.SetValue(NameProperty, 'X');
+            a2.Content = gameLogicLayer.myTurn;
+        }
+
+        private void a3_Click(object sender, RoutedEventArgs e)
+        {
+            //a1.SetValue(NameProperty, 'X');
+            a3.Content = gameLogicLayer.myTurn;
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            //a1.SetValue(NameProperty, 'X');
+            b1.Content = gameLogicLayer.myTurn;
+        }
+
+        private void b2_Click(object sender, RoutedEventArgs e)
+        {
+            //a1.SetValue(NameProperty, 'X');
+            b2.Content = gameLogicLayer.myTurn;
+        }
+
+        private void b3_Click(object sender, RoutedEventArgs e)
+        {
+            //a1.SetValue(NameProperty, 'X');
+            b3.Content = gameLogicLayer.myTurn;
+        }
+
+        private void c1_Click(object sender, RoutedEventArgs e)
+        {
+            //a1.SetValue(NameProperty, 'X');
+            c1.Content = gameLogicLayer.myTurn;
+        }
+
+        private void c2_Click(object sender, RoutedEventArgs e)
+        {
+            //a1.SetValue(NameProperty, 'X');
+            c2.Content = gameLogicLayer.myTurn;
+        }
+
+        private void c3_Click(object sender, RoutedEventArgs e)
+        {
+            //a1.SetValue(NameProperty, 'X');
+            c3.Content = gameLogicLayer.myTurn;
+        }
     }
 }
--- TicTacToe/GameLogicLayer.cs
+++ TicTacToe/GameLogicLayer.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace TicTacToe
 {
     public class GameLogicLayer
     {
         private ConnectionHandler cHandler;
+        private string opponnentNickname;
+        private int opponnentDescriptor;
+        private int isMyTurn;
+        public char myTurn { get; set; }
 
         public GameLogicLayer()
         {
@@ -21,9 +27,55 @@ namespace TicTacToe
             cHandler.SendData("1 " + login);
             //int status = Int32.Parse(cHandler.Receive());
             string response = cHandler.Receive();
-            Console.WriteLine(response);
+            string[] splittedResponse;
+            
+            splittedResponse = response.Split(' ');
+
+            opponnentNickname = splittedResponse[0];
+            opponnentDescriptor = Int32.Parse(splittedResponse[1]);
+            isMyTurn = Int32.Parse(splittedResponse[2]);
+
+            Console.WriteLine("nick przeciwnika to: " + opponnentNickname);
+            Console.WriteLine("deskryptor przeciwnika to: " + opponnentDescriptor);
+            Console.WriteLine("moja kolejnosc? : " + isMyTurn);
+
+            if (isMyTurn == 1)
+            {
+                myTurn = 'O';
+                MessageBox.Show("Twoj ruch to " + myTurn);
+            }
+            else
+            {
+                myTurn = 'X';
+                MessageBox.Show("Twoj ruch to " + myTurn);
+            }
+
+            //if (isMyTurn == 1)
+            //{
+
+            //}
+            //else
+            //{
+
+            //}
 
             //return status;
+        }
+
+        public void sendClickedField(string field)
+        {
+            if(isMyTurn == 1)
+            {
+                cHandler.SendData("2 " + field + " " + opponnentDescriptor);
+                isMyTurn = 0;
+            }
+            else
+            {
+                string opponnentTurn = cHandler.Receive();
+
+                Console.WriteLine(opponnentTurn);
+                isMyTurn = 1;
+            }
         }
 
         public void Connect(string ip)
